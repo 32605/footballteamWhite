e8f2a761e0cd082fcba474aa3521e08bf685f794
32605 <116140798+32605@users.noreply.github.com>
2022-11-30 12:11:47 +0700

    Add files via upload

---
+++ footballteamWhite.py
@@ -0,0 +1,19 @@
+football_team = []
+
+#append 11 players name
+for i in range(11):
+    name = input("Add a player to football team: ")
+    football_team.append(name)
+print(football_team)
+
+repeat = 'Y'
+while repeat == 'Y'
+    edit = int(input("Which player do you want to change?: "))
+    football_team[edit-1] = input("Enter a new player: ")
+    print(football_team)
+
+    delete = int(input("Which player do you want to change?: "))
+    del football_team[delete-1]
+    print(football_team)
+    
+     repeat = input('Do you want to edit or delete name(Y/N)')
